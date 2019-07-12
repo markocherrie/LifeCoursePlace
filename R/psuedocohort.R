c79a367e243867ec00cbc621883ccf6cd0926c18
CHERRIE Mark <mcherrie@ed.ac.uk>
2019-07-12 15:29:18 +0100

    Fixed the arguments for the function

--- R/psuedocohort.R
+++ R/psuedocohort.R
@@ -7,15 +7,15 @@
 #' @param longmax maximum longitude of bounding box
 #' @param latmin minimum latitude of bounding box
 #' @param latmax maximum longitude of bounding box
-#' @param numberofaddresses How many addresses to try and sample
+#' @param numberofaddresses How many addresses to try and sample (<1000)
 #' @return A dataframe of addresses in bounding box
 #' @export
 #'
 
-pseudocohort<-function(longmin, longmax, latmin, latmax, numberinsample){
-sample<-data.frame(cbind(runif(1000,-3.316587,-3.071410),runif(1000,55.891648,55.991446)))
+pseudocohort<-function(longmin, longmax, latmin, latmax, numberofaddresses){
+sample<-data.frame(cbind(runif(1000,longmin,longmax),runif(1000,latmin,latmax)))
 colnames(sample)<-c("long", "lat")
-sample_addresses <- photon::reverse(x = sample$long[1:100], y = sample$lat[1:100])
+sample_addresses <- photon::reverse(x = sample$long[1:numberofaddresses], y = sample$lat[1:numberofaddresses])
 sample_addresses<-sample_addresses[sample_addresses$osm_value=="house",]
 sample_addresses<-sample_addresses[!is.na(sample_addresses$x),]
 sample_addresses
