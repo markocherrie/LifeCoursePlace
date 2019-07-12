bb91c0bccf57d7dd75346867065fbde3d5e0bc67
CHERRIE Mark <mcherrie@ed.ac.uk>
2019-07-12 17:30:29 +0100

    Added the other parks datasets to the package

---
+++ R/EdinburghParks1969.R
@@ -0,0 +1,20 @@
+#' Edinburgh parks data from 1969
+#'
+#' Data from the MMP project
+#' Public parks in Edinburgh were digitised from historic maps
+#' @docType data
+#'
+#' @usage data(EdinburghParks1969)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Pearce et al. (2016) IJERPH 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/26999179}{NCBI})
+#'
+#' @source \href{https://www.ncbi.nlm.nih.gov/pubmed/26999179}{QTL Archive}
+#'
+#' @examples
+#' data(EdinburghParks1969)
+NULL
