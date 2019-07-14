eb28ec3666be978d7186cf3c035654c9e3af5a2a
CHERRIE Mark <mcherrie@ed.ac.uk>
2019-07-14 21:45:36 +0100

    first commit for buffer exposure function

---
+++ R/ana_bufferexposure.R
@@ -0,0 +1,15 @@
+#' Generate greenspace buffers
+#'
+#' This function generates greenspace 'exposure' for participants
+#'
+#' @param lat
+#' @param lon
+#' @param id
+#' @param year
+#' @param buffer
+#' @return A dataframe of addresses with buffered exposure
+#' @export
+#'
+#'
+#'U:\Datastore\CSCE\geos\groups\mmplbc\MPCC_MMP_2018\Paper7_traj\clean!!
+#'
