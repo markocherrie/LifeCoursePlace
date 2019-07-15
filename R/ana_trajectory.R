a7f5e8ae7d4ca9d70da26aba576554cf061d6f19
markocherrie <mark.cherrie.mc@gmail.com>
2019-07-15 22:11:33 +0100

    fist commit trajectory analysis

---
+++ R/ana_trajectory.R
@@ -0,0 +1,15 @@
+#' Analyse greenspace trajectory
+#'
+#' This function takes exposure estimates for a number of years
+#' and returns individuals grousped into classes of similar trajectory
+#'
+#' @param lat
+#' @param lon
+#' @param id
+#' @param year
+#' @param buffer
+#' @return A dataframe of trajectory classes
+#' @export
+#'
+#'
+#'
